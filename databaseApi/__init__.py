e0e983c7d7f23b46f07f1649276c0b5a41f02135
Kevin3467 <kevin_3467@yahoo.com>
2022-04-10 21:27:08 -0300

    configurando dbs de produção e de desenvolvimento

--- databaseApi/__init__.py
+++ databaseApi/__init__.py
@@ -5,6 +5,9 @@ from flask_jwt_extended import (JWTManager)
 app = Flask(__name__, template_folder='.')
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gicnsnsixmbhqs:2fa8fe4eb2f869061d5adecee9d2ba09dafdb5386ea6a52ac1fdb701c17227c3@ec2-52-70-186-184.compute-1.amazonaws.com:5432/dfnhlth983shho'
+#db local para debug
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 app.config['JWT_SECRET_KEY'] = '!0projetacsVision'
 jwt = JWTManager(app)
 
