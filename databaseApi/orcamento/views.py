b92e7f933aaea25eb0108f5e2f7c77ca33a07a4a
kevin3467 <kevin_3467@yahoo.com>
2022-05-19 10:45:35 -0300

    endpoint read all orçamentos

--- databaseApi/orcamento/views.py
+++ databaseApi/orcamento/views.py
@@ -1,5 +1,4 @@
 from flask import request, Blueprint
-import json
 from databaseApi import db
 from databaseApi.main import gera_response, cross_origin
 from databaseApi.models import Ctrl_orcamentos
@@ -8,13 +7,23 @@ from flask_jwt_extended import jwt_required
 orcamento = Blueprint('orcamento', __name__)
 
 
+
+def Tupple_to_json(obj,json):
+    OrcNum = len(obj)
+    for item in range(OrcNum):
+        dict_ = dict(zip(('Nome','Descrição','Codigo'), obj[item]))
+        json.append(dict_)
+    
+
+    
+
 @orcamento.route('/read/orcamentos', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 @jwt_required()
 def Orcamento():
 
-    orcamento_obj = Ctrl_orcamentos.query.all()
-    orcamento_json = [orcamento.to_json() for orcamento in orcamento_obj]
+    orcamento_obj = db.session.query(Ctrl_orcamentos.orcnome, Ctrl_orcamentos.orcdescricao, Ctrl_orcamentos.orccodigo).all()
+    orcamento_json = []
+    Tupple_to_json(orcamento_obj,orcamento_json)
 
-
-    return gera_response(200,orcamento_json)
+    return gera_response(200, orcamento_json)
