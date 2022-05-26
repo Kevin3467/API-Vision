6c8b614d7ae32d282c1ef7ff7bd78cb76a2d4785
kevin3467 <kevin_3467@yahoo.com>
2022-05-26 11:54:29 -0300

    filtro do bmsa juntando contrato

--- databaseApi/chamados/views.py
+++ databaseApi/chamados/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from databaseApi import db
 from databaseApi.main import gera_response, cross_origin, Tupple_to_json
 from databaseApi.models import Ctrl_chamados,Ctrl_contrato
+from sqlalchemy import or_
 from flask_jwt_extended import jwt_required
 
 chamados = Blueprint('chamados', __name__)
@@ -19,6 +20,27 @@ def Chamados(idcnt):
         ).filter(
         Ctrl_chamados.idcnt == Ctrl_contrato.id,
         Ctrl_contrato.id == idcnt
+        ).all()
+
+    cols_title = ('id','idcnt','Titulo')
+    Chamados_json = []
+    Tupple_to_json(Chamados_obj,Chamados_json,cols_title)
+    
+
+    return gera_response(200, Chamados_json)
+
+
+# lista todos os chamados na tabela
+@chamados.route('/read/chamados/bmsa', methods=['GET'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@jwt_required()
+def Chamados_bmsa():
+    
+    Chamados_obj = db.session.query(
+        Ctrl_chamados.id, Ctrl_contrato.id, Ctrl_chamados.chmTitulo
+        ).filter(
+        Ctrl_chamados.idcnt == Ctrl_contrato.id,
+        or_(Ctrl_contrato.id == 6, Ctrl_contrato.id == 7)
         ).all()
 
     cols_title = ('id','idcnt','Titulo')
