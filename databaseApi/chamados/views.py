28d35c58c8443496feb9acd248bf1f599464e143
kevin3467 <kevin_3467@yahoo.com>
2022-06-06 14:02:30 -0300

    recuperando ajustes no corst

--- databaseApi/chamados/views.py
+++ databaseApi/chamados/views.py
@@ -119,7 +119,7 @@ def create_chamados():
 
 
 # Atualizar Orçamento
-@chamados.route("/chamados/<id>", methods=['PUT','OPTIONS'])
+@chamados.route("/chamados/<id>", methods=["PUT"])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 @jwt_required()
 def AtualizaChamados(id):
