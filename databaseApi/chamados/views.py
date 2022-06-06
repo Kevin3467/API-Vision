782efa8deef7faea0dfe89d414a8601d61357f11
kevin3467 <kevin_3467@yahoo.com>
2022-06-06 15:14:26 -0300

    dj azeitona

--- databaseApi/chamados/views.py
+++ databaseApi/chamados/views.py
@@ -120,7 +120,7 @@ def create_chamados():
 
 # Atualizar Orçamento
 @chamados.route("/chamados/<id>", methods=["PUT"])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'],methods=['PUT,OPTIONS'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'],methods=['PUT,OPTIONS,DELETE'])
 @jwt_required()
 def AtualizaChamados(id):
 
