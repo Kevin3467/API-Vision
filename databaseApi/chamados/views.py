56cd241c2354912cd1736701bb305b1554957ed8
kevin3467 <kevin_3467@yahoo.com>
2022-06-06 15:10:39 -0300

    me ajuda pfv

--- databaseApi/chamados/views.py
+++ databaseApi/chamados/views.py
@@ -120,7 +120,7 @@ def create_chamados():
 
 # Atualizar Orçamento
 @chamados.route("/chamados/<id>", methods=["PUT"])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'],methods=['PUT,OPTION'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'],methods=['PUT,OPTIONS'])
 @jwt_required()
 def AtualizaChamados(id):
 
