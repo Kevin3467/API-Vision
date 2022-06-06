fcb5cc5d34f8e3171f04fff4a867a9b50addae82
kevin3467 <kevin_3467@yahoo.com>
2022-06-06 16:11:56 -0300

    vaitomarnocu

--- databaseApi/chamados/views.py
+++ databaseApi/chamados/views.py
@@ -119,8 +119,8 @@ def create_chamados():
 
 
 # Atualizar Orçamento
-@chamados.route("/chamados/<id>", methods=["POST","OPTIONS","DELETE"])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@chamados.route("/chamados/<id>", methods=["PUT","OPTIONS","DELETE"])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'], methods='*')
 @jwt_required()
 def AtualizaChamados(id):
 
